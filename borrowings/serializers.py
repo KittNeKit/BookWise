fcdaae22ef075793f4c1b2b27d0ab82f81367097
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-06 19:44:31 +0300

    Added create method for borrowing serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,4 +1,6 @@
+from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from books.serializers import BooksSerializer
 from borrowings.models import Borrowing
@@ -8,12 +10,19 @@ class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = (
+            "id",
             "borrow_date",
             "expected_return_date",
             "actual_return_date",
             "book_id",
             "user_id",
         )
+
+    @transaction.atomic()
+    def create(self, validated_data):
+        validated_data["book_id"].inventory -= 1
+        validated_data["book_id"].save()
+        return Borrowing.objects.create(**validated_data)
 
 
 class BorrowingDetailSerializer(BorrowingSerializer):
