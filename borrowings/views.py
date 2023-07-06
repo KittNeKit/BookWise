9b1237572ff9aa958984327c15bc3225098a7eb4
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-06 20:24:06 +0300

    Added permission and authentication classes for borrowing view set

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
@@ -7,6 +9,8 @@ from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerialize
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("book_id", "user_id")
     serializer_class = BorrowingSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return Borrowing.objects.filter(user_id=self.request.user.id)
