811c1cbb77cbbb96a223a8af229d154f25f443e8
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-06 19:03:09 +0300

    Update datefields for borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -5,8 +5,8 @@ from user.models import User
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateTimeField()
+    borrow_date = models.DateTimeField(auto_now_add=True)
     expected_return_date = models.DateTimeField()
-    actual_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField(blank=True)
     book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
