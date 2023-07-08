324165020f638b50325785da437483530c752590
Nikita Provorov <nikiyorkovich@gmail.com>
2023-07-08 16:32:28 +0300

    Implement stripe create session to borrowing create serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,9 +1,12 @@
+from decimal import Decimal
+
 from django.db import transaction
 from rest_framework import serializers
 
 from books.serializers import BooksSerializer
 from borrowings.models import Borrowing, Payment
 from borrowings.notification import send_borrowing_create_message
+from borrowings.stripe import create_stripe_session
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
@@ -24,11 +27,25 @@ class BorrowingSerializer(serializers.ModelSerializer):
 
         borrowing = Borrowing.objects.create(**validated_data)
 
+        stripe_session = create_stripe_session(
+            borrowing=borrowing,
+            is_fine=False
+        )
+
+        Payment.objects.create(
+            status="PENDING",
+            type="PAYMENT",
+            borrowing_id=borrowing,
+            session_url=stripe_session["url"],
+            session_id=stripe_session["id"],
+            to_pay=Decimal(stripe_session["amount_total"] / 100),
+        )
+
         send_borrowing_create_message(
             user=validated_data["user_id"],
             borrowing=borrowing
         )
-
+        print(stripe_session["payment_status"])
         return borrowing
 
 
